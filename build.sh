c80bd3f1a185e04ee3baf5ba79e5e2d9cdf6c9bf
sowawa <keisuke.sogawa@gmail.com>
2014-09-28 15:22:29 +0900

    Update build.sh for importing rpm-gpg-key

--- build.sh
+++ build.sh
@@ -18,9 +18,22 @@ sudo docker cp $CONTAINER_ID:/etc/system-release .
 # VERSION=`cat system-release | grep -o -e 20[1-9][0-9]\.[0-9][0-9] | sed -e 's/\.//'`
 VERSION=`cat system-release | grep -o -e 20[1-9][0-9]\.[0-9][0-9]`
 
-IMAGE_ID=`cat amazon-linux.tar | docker import - sowawa/amazon-linux`
-docker tag $IMAGE_ID sowawa/amazon-linux:${VERSION}_`date +"%Y%m%d"`
+cat amazon-linux.tar | docker import - sowawa/amazon-linux
 
 test -e ./amazon-linux.tar && rm ./amazon-linux.tar
 test -e ./build.cpid && rm ./build.cpid
 test -e ./system-release && rm ./system-release
+
+test -e ./Dockerfile && rm ./Dockerfile
+cat << _EOF_ > Dockerfile 2>&1
+From sowawa/amazon-linux
+MAINTAINER sowawa <keisuke.sogawa@gmail.com>
+
+RUN /bin/rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-amazon-ga
+RUN /bin/rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-amazon-beta
+CMD 'echo'
+_EOF_
+
+docker build -t sowawa/amazon-linux .
+docker tag sowawa/amazon-linux:latest sowawa/amazon-linux:${VERSION}_`date +"%Y%m%d"`
+test -e ./Dockerfile && rm ./Dockerfile
